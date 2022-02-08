cfa73e0fe3672556d9ec09ac7ceef5f8a4216dd2
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-02-08 16:39:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res, next) => {
       res.json({error: err.message})
     } else {
       console.log(result);
-      res.send(`Data:<br>${JSON.stringify(result.rows).replace('\n', '<br>').replace(' ', '&nbsp;')}`);
+      res.send(`<span style="font-family:"Helvetica";">Data:<br>${JSON.stringify(result.rows, null, 2).replace('\n', '<br>').replace(' ', '&nbsp;')}</span>`);
     }
   });
 });
