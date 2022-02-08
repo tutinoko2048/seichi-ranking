b83891141cce83d63400c286f50578722099c4da
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-02-08 10:46:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const server = app.listen(PORT, () => {
 });
 const moment = require('moment-timezone');
 moment.tz.setDefault('Asia/Tokyo');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+const db = require('../db/index');
 
 app.use(express.static('public'));
 
 app.get("/", (req, res, next) => {
   res.sendFile(__dirname + '/public/index.html');
+  
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query('SELECT player, mine, level FROM serverdata', (err, result) => {
+        console.log(result);
+      });
+    }
+  });
+  
 });
 
 function getTime() {
